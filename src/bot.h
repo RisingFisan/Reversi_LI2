d0284fbf3bd571077732c83a34c80d477860e60c
RisingFisan <sofiarsantos31@gmail.com>
2019-04-29 23:40:06 +0100

    Documentação

--- src/bot.h
+++ src/bot.h
@@ -1,8 +1,11 @@
 #include "estado.h"
 
+/**
+ * Estrutura que armazena as características de um 'bot', i.e., um jogador controlado pelo computador.
+ */
 typedef struct bot {
-    char dif;
-    VALOR peca;
+    char dif; ///< a dificuldade do bot
+    VALOR peca; ///< a peça com que o bot joga
 } BOT;
 
 int jogadaBot(BOT* bot, ESTADO* e);
--- src/estado.h
+++ src/estado.h
@@ -15,31 +15,54 @@ Definição do estado i.e. tabuleiro. Representação matricial do tabuleiro.
 */
 
 
-// definição de valores possiveis no tabuleiro
+/** definição de valores possiveis no tabuleiro 
+@param VAZIA casa vazia
+@param VALOR_X casa ocupada por uma peça X
+@param VALOR_O casa ocupada por uma peça O
+*/
 typedef enum {VAZIA, VALOR_X, VALOR_O} VALOR;
 
-/**
-Estrutura que armazena o estado do jogo
-*/
+/*!
+ * @brief Estrutura que armazena o estado do jogo
+ * 
+ * Um estado contém todas as informações do jogo, em especial o tabuleiro. Assim, para guardar o jogo, basta apenas guardar o estado atual.
+ */
 typedef struct estado {
-    VALOR peca; // peça do jogador que vai jogar!
-    VALOR grelha[8][8];
-    char modo; // modo em que se está a jogar! 0-> manual, 1-> contra computador
-    char dif; // dificuldade do jogo (quando em modo automático), entre 1 e 3.
+    VALOR peca; ///< peça do jogador que vai jogar!
+    VALOR grelha[8][8]; ///< tabuleiro do jogo
+    char modo; ///< tipo de jogo! 0-> manual, 1-> contra o computador
+    char dif; ///< dificuldade do jogo (quando em modo automático, contra o computador), entre 1 e 3.
 } ESTADO;
 
+/**
+Estrutura dinâmica (lista ligada) que armazena o histórico do jogo, i.e., todas as jogadas anteriores.
+@param e estado armazenado na lista
+@param prox apontador para o resto da lista
+*/
 typedef struct estadosh {
     ESTADO e;
     struct estadosh * prox;
 } *ESTADOSH;
 
-// Armazena uma posição da grelha.
+/**
+Estrutura que armazena uma posição da grelha.
+@param lin linha da posição
+@param col coluna da posição
+@param dest "poder destrutivo" da posição, i.e., quantas peças rivais uma peça "come" se for colocada nessa posição
+*/
 typedef struct posicao {
     char lin;
-    char col;
+    char col; 
     char dest;
 } POSICAO;
 
+/**
+Imprime no terminal o tabuleiro do jogo, com as peças no seu lugar.
+@param e estado onde está guardado o tabuleiro.
+@param pVal se for 1, indica com um '.' as posições onde o jogador atual pode jogar.
+@param nPos número de posições válidas, i.e., onde o jogador atual pode jogar.
+@param pos lista com as posições válidas para o jogador atual.
+ */
 void printa(ESTADO e, int* pVal, int nPos, POSICAO pos[]);
 
 void newBoard(ESTADO* e, VALOR peca, char modo);
